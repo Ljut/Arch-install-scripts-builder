3bc8bf648b929b88b5268ed0d440f10df5251d34
ljut <ljutikazlatan@gmail.com>
2025-02-22 17:50:37 +0100

    setting up install for KDE

--- configs/postinstall.sh
+++ configs/postinstall.sh
@@ -1,1 +1,6 @@
 # Put here configurations and commands to run after base install
+systemctl enable NetworkManager.service
+systemctl enable avahi-daemon.service
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
